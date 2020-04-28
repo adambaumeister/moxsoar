bf1de28a9e3f06392e977d73a9d492a2db03c0ab
Adam Baumeister <abigserve@gmail.com>
2020-04-28 22:55:03 +1000

    Fix URL pathing and use httpMUX to allow for concurrent http servers to be spun up

--- integrations/base.go
+++ integrations/base.go
@@ -1,7 +1,29 @@
 package integrations
 
+import "regexp"
+
 type BaseIntegration struct {
 	Routes map[string]Route
+}
+
+func (bi *BaseIntegration) GetRoute(url string) Route {
+	if route, ok := bi.Routes[url]; ok {
+		return route
+	}
+
+	for _, route := range bi.Routes {
+		m, _ := regexp.MatchString(route.Path, url)
+		if m {
+			return route
+		}
+	}
+
+	// This is pretty hacky for now, need to improve this
+	return Route{
+		Path:         "/",
+		ResponseFile: "default.json",
+		ResponseCode: 200,
+	}
 }
 
 type Route struct {
--- integrations/minemeld/responder.go
+++ integrations/minemeld/responder.go
@@ -28,9 +28,13 @@ func (i *Minemeld) Start(contentDir string, addr string) {
 		log.Fatal(err)
 	}
 
+	httpMux := http.NewServeMux()
 	for _, route := range i.Routes {
-		http.HandleFunc(route.Path, func(writer http.ResponseWriter, request *http.Request) {
-			fb, err := ioutil.ReadFile(path.Join(contentDir, "minemeld", route.ResponseFile))
+		// This doesn't work - The routes overwrite the other ones
+		httpMux.HandleFunc(route.Path, func(writer http.ResponseWriter, request *http.Request) {
+			// HandleFunc gets defined when the server starts, dispatch runs when a request is received
+			r := i.Dispatch(request, contentDir)
+			fb, err := ioutil.ReadFile(path.Join(contentDir, "minemeld", r.ResponseFile))
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -39,9 +43,15 @@ func (i *Minemeld) Start(contentDir string, addr string) {
 
 	}
 
-	err = http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, httpMux)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func (m *Minemeld) Dispatch(request *http.Request, contentdir string) integrations.Route {
+	// Used at runtime
+	r := m.BaseIntegration.GetRoute(request.URL.Path)
+	return r
 }
